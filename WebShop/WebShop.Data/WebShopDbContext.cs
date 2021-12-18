9c6c65989e23c70d7fdfb9fd0da6f270b0e8963c
ValeriaKey <valeriakolossova@outlook.com>
2021-12-19 00:22:12 +0300

    WebShopDbContext + Product

---
+++ WebShop/WebShop.Data/WebShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Data
+{
+    public class WebShopDbContext : DbContext
+    {
+        public WebShopDbContext(DbContextOptions<WebShopDbContext> options)
+            : base(options) { }
+    }
+}
--- WebShop/WebShop/Startup.cs
+++ WebShop/WebShop/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebShop.Data;
 
 namespace WebShop
 {
@@ -15,15 +17,18 @@ namespace WebShop
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration _config { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<WebShopDbContext>(options =>
+                options.UseSqlServer(_config["DefaultConnetion"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
